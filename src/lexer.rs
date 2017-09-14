dd907604833e0210a31e50514e1dc81c63ba36f8
Saethlin <kimockb@gmail.com>
2017-09-14 16:57:10 -0400

    Refactor lexer to achieve blazing speed
    Replaced iterator cloning + nth with while let, removed recursion to deal with comments in favor of a huge while let, modified tokens to carry the data they need with them

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,96 +1,73 @@
 use std::str::Chars;
-use std::iter::Peekable;
-use std::ffi::CString;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     EOF,
     Definition,
     Extern,
-    Identifier,
-    Number,
-    Punctuation,
+    Identifier(String),
+    Number(f64),
+    Punctuation(char),
 }
 
 pub struct Lexer<'a> {
-    pub current_char: char,
-    pub identifier: String,
-    numeric: String,
-    pub numeric_value: f64,
-    getchar: Peekable<Chars<'a>>,
+    buffer: String,
+    getchar: Chars<'a>,
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Lexer {
         Lexer {
-            current_char: 0 as char,
-            identifier: String::new(),
-            numeric: String::new(),
-            numeric_value: 0.0,
-            getchar: input.chars().peekable(),
+            buffer: String::new(),
+            getchar: input.chars(),
         }
-    }
-
-    pub fn identifier(&self) -> CString {
-        let mut data = self.identifier.chars().map(|c| c as i8).collect::<Vec<_>>();
-        data.push(0);
-        CString::from_raw(data.as_mut_ptr())
     }
 
     pub fn next_token(&mut self) -> Token {
-        self.identifier.clear();
-        self.numeric.clear();
+        self.buffer.clear();
 
-        let mut c = self.getchar.next();
-        while c.is_some() && c.unwrap().is_whitespace() {
-            c = self.getchar.next();
-        }
-
-        match c {
-            Some(c) => {
-                self.current_char = c;
-
-                // Eat an identifier
-                if c.is_alphabetic() {
-                    self.identifier.push(c);
-                    self.identifier.extend(self.getchar.clone().take_while(
-                        |c| c.is_alphabetic() || c.is_numeric(),
-                    ));
-                    if self.identifier.len() > 1 {
-                        self.getchar.nth(self.identifier.len() - 2);
-                    }
-
-                // Eat a numeric literal
-                } else if c.is_numeric() || c == '.' {
-                    self.numeric.push(c);
-                    self.numeric.extend(self.getchar.clone().take_while(
-                        |c| c.is_numeric() || *c == '.',
-                    ));
-                    self.numeric_value = self.numeric.parse().unwrap();
-                    if self.numeric.len() > 1 {
-                        self.getchar.nth(self.numeric.len() - 2);
-                    }
-
-
-                // Eat comments
-                } else if c == '#' {
-                    self.getchar.position(|c| c == '\n');
-                    return self.next_token();
-                };
-
-                if self.identifier.as_str() == "def" {
-                    Token::Definition
-                } else if self.identifier.as_str() == "extern" {
-                    Token::Extern
-                } else if !self.identifier.is_empty() {
-                    Token::Identifier
-                } else if !self.numeric.is_empty() {
-                    Token::Number
-                } else {
-                    Token::Punctuation
-                }
+        while let Some(c) = self.getchar.next() {
+            if c.is_whitespace() {
+                continue;
             }
-            None => Token::EOF,
+            // Comments
+            if c == '#' {
+                self.getchar.find(|&c| c == '\n');
+                continue;
+            }
+            // Identifier
+            if c.is_alphabetic() {
+                self.buffer.push(c);
+                while let Some(c) = self.getchar.next() {
+                    if c.is_alphabetic() || c.is_numeric() {
+                        self.buffer.push(c);
+                    } else {
+                        break;
+                    }
+                }
+                return match self.buffer.as_str() {
+                    "def" => Token::Definition,
+                    "extern" => Token::Extern,
+                    _ => Token::Identifier(self.buffer),
+                };
+            // Numeric literal
+            } else if c.is_numeric() || c == '.' {
+                self.buffer.push(c);
+                while let Some(c) = self.getchar.next() {
+                    if c.is_numeric() || c == '.' {
+                        self.buffer.push(c)
+                    } else {
+                        break;
+                    }
+                }
+                return match self.buffer.parse() {
+                    Ok(number) => Token::Number(number),
+                    Err(_) => panic!("Invalid numeric literal {}", self.buffer),
+                };
+            } else {
+                return Token::Punctuation(c);
+            }
         }
+        Token::EOF
     }
 }
